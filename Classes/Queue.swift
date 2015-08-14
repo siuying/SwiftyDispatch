fba072a9de9ea15014cd015c6c5191ddd80b913e
Francis Chong <francis@ignition.hk>
2015-08-14 23:29:21 +0800

    Fix after: wrong constant means we wait millseconds instead of seconds

--- Classes/Queue.swift
+++ Classes/Queue.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public class Queue {
+public struct Queue {
     public let queue : dispatch_queue_t
     
     public enum QueueType {
@@ -79,10 +79,17 @@ public class Queue {
         }
     }
 
+    /// Create a Queue object with a dispatch_queue_t
+    ///
+    /// - Parameter queue: underlying dispatch_queue_t object
     public init(queue: dispatch_queue_t) {
         self.queue = queue
     }
-    
+
+    /// Create a Queue with label and type
+    ///
+    /// - Parameter label: label of the queue
+    /// - Parameter type: Type of the queue (QueueType)
     public init(_ label: String, _ type: QueueType = .Serial) {
         let labelBytes : UnsafePointer<Int8> = label.cStringUsingEncoding(NSUTF8StringEncoding)!.withUnsafeBufferPointer({
             $0.baseAddress
@@ -90,32 +97,51 @@ public class Queue {
         self.queue = dispatch_queue_create(labelBytes, type.value)
     }
 
+    /// Run a block asynchronously
+    ///
+    /// - Parameter block: The block to run
     public func async(block: dispatch_block_t) {
         dispatch_async(self.queue, block)
     }
-    
+
+    /// Run a block synchronously
+    ///
+    /// - Parameter block: The block to run
     public func sync(block: dispatch_block_t) {
         dispatch_sync(self.queue, block)
     }
-    
+
+    /// Submits a block to run for multiple times.
+    ///
+    /// - Parameter iterations: Number of iterations
+    /// - Parameter block: The block to run
     public func apply(iterations: Int, block: (Int) -> Void) {
         dispatch_apply(iterations, queue, block)
     }
 
+    /// Schedule a block at a specified time.
+    ///
+    /// - Parameter delayInSeconds: Delay in number of seconds
+    /// - Parameter block: The block to run
     public func after(delayInSeconds: Double, block: dispatch_block_t) {
-        let nanoSeconds = Int64(Double(NSEC_PER_MSEC) * delayInSeconds)
+        let nanoSeconds = Int64(Double(NSEC_PER_SEC) * delayInSeconds)
         let delayTime = dispatch_time(DISPATCH_TIME_NOW, nanoSeconds)
         dispatch_after(delayTime, queue, block)
     }
-    
+
+    /// Returns the default queue that is bound to the main thread.
     public static func main() -> Queue {
         return Queue(queue: dispatch_get_main_queue())
     }
-    
+
+    /// Returns a global queue by QOS
+    /// - Parameter qos: the QOS of the queue
     public static func concurrent(qos: Queue.QOS) -> Queue {
         return Queue(queue: dispatch_get_global_queue(qos.value, 0))
     }
 
+    /// Returns a global queue by Priotity
+    /// - Parameter priority: the priority of the queue
     public static func concurrent(priority priority: Queue.Priority) -> Queue {
         return Queue(queue: dispatch_get_global_queue(priority.value, 0))
     }
